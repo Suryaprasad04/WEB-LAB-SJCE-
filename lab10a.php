459ed43e60960c39ffb122ef0d3790904d5eb161
SURYAPRASAD L <150020783+Suryaprasad04@users.noreply.github.com>
2025-06-27 18:33:41 +0530

    Create lab10a.php

---
+++ lab10a.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Today's Date</title>
+</head>
+<body>
+
+    <h2>Today's Date</h2>
+    <?php
+        echo "Date: " . date("d-m-Y");
+    ?>
+
+</body>
+</html>
